055091a9531692179c64d27f6ca787cc55855600
Rahul Papneja <rahul.papneja@livfin.com>
2021-01-06 14:26:13 +0530

    Hibernate configuration file and main class connection establish lesson 2

--- hibernate/HibernateMavenProject/src/main/java/com/rahul/App.java
+++ hibernate/HibernateMavenProject/src/main/java/com/rahul/App.java
@@ -1,13 +1,23 @@
 package com.rahul;
 
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws HibernateException
     {
         System.out.println( "Hello World!" );
+		/*
+		 * Configuration cfg=new Configuration(); cfg.configure();
+		 */
+        SessionFactory factory= new Configuration().configure().buildSessionFactory();
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
     }
 }
