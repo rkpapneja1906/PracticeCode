74d42845ed2675c8dfbf93592358e89b2c01a441
Rahul Papneja <rahul.papneja@livfin.com>
2021-01-08 23:39:43 +0530

    Basic Annotations for Hibernate

--- hibernate/HibernateMavenProject/src/main/java/com/rahul/App.java
+++ hibernate/HibernateMavenProject/src/main/java/com/rahul/App.java
@@ -1,5 +1,10 @@
 package com.rahul;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Date;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -12,7 +17,7 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
-    public static void main( String[] args ) throws HibernateException
+    public static void main( String[] args ) throws HibernateException, IOException
     {
         System.out.println( "Hello World!" );
 		/*
@@ -22,20 +27,31 @@ public class App
        // System.out.println(factory);
         //System.out.println(factory.isClosed());
         Student st=new Student();
-        st.setId(101);
+        st.setId(105);
         st.setName("Rahul");
         st.setCity("Panipat");
 
         System.out.println(st);
-        Student st1=new Student();
-        st1.setId(102);
-        st1.setName("Rahul Papneja");
-        st1.setCity("Panipat_Panipat");
+		/*
+		 * Student st1=new Student(); st1.setId(102); st1.setName("Rahul Papneja");
+		 * st1.setCity("Panipat_Panipat");
+		 */
 
+        Address address=new Address();
+        address.setStreet("Ram Nagar");
+        address.setCity("Panipat");
+        address.setOpen(true);
+        address.setX(22.7);
+        address.setAddedDate(new Date());
+        FileInputStream fis=new FileInputStream("src/main/java/index.jpg");
+        byte[] data=new byte[fis.available()];
+        fis.read(data);
+        address.setImage(data);
         Session session= factory.openSession();
         Transaction tx=session.beginTransaction();        
         session.save(st);
-        session.save(st1);
+        session.save(address);
+       // session.save(st1);
         tx.commit();
         //Transaction tx1=session.beginTransaction();        
         
