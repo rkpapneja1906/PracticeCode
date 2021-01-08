6af58091f586346ec7e9ce4a298f88b54216e2bd
Rahul Papneja <rahul.papneja@livfin.com>
2021-01-08 22:57:15 +0530

    Connection establish and table creation and data insert using Hibernate

--- hibernate/HibernateMavenProject/src/main/java/com/rahul/App.java
+++ hibernate/HibernateMavenProject/src/main/java/com/rahul/App.java
@@ -1,7 +1,9 @@
 package com.rahul;
 
 import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -17,7 +19,30 @@ public class App
 		 * Configuration cfg=new Configuration(); cfg.configure();
 		 */
         SessionFactory factory= new Configuration().configure().buildSessionFactory();
-        System.out.println(factory);
-        System.out.println(factory.isClosed());
+       // System.out.println(factory);
+        //System.out.println(factory.isClosed());
+        Student st=new Student();
+        st.setId(101);
+        st.setName("Rahul");
+        st.setCity("Panipat");
+
+        System.out.println(st);
+        Student st1=new Student();
+        st1.setId(102);
+        st1.setName("Rahul Papneja");
+        st1.setCity("Panipat_Panipat");
+
+        Session session= factory.openSession();
+        Transaction tx=session.beginTransaction();        
+        session.save(st);
+        session.save(st1);
+        tx.commit();
+        //Transaction tx1=session.beginTransaction();        
+        
+        //tx1.commit();        // session.getTransaction().commit();
+        session.close();
+        
+        
+        
     }
 }
